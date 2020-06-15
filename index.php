6f7cff624f3d7b066cb294ce39d82b8c0e653eff
maikel abdelmalak <maikel22@hotmail.it>
2020-06-15 16:48:12 +0200

    css

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
-    $parola_dasostituire = 'sed';
+
+    $parola_dasostituire = $_GET["badwords"];
     $sostituzione = '***';
     $testo = 'Lorem ipsum dolor sit amet, sed consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis sed nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint sed occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est sed laborum.';
     // echo $testo;
     $finale = str_replace($parola_dasostituire, $sostituzione, $testo);
     // echo $finale;
+    $lunghezza = strlen($testo);
  ?>
- <p>
-     <?php echo $finale; ?>
- </p>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <link rel="stylesheet" href="../style.css">
+        <title>php</title>
     </head>
     <body>
-
+        <div>
+            <p>
+                <?php echo $finale; ?>
+            </p>
+            <span>questo paragrafo ha <?php echo $lunghezza; ?> caratteri</span>
+        </div>
     </body>
 </html>
